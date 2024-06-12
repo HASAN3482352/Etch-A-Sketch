d285cf6e88b0bcca70f53b46bc917d866c35ecc3
Hasan Iqbal Khan <hik77920@gmail.com>
2024-06-12 11:33:01 +0530

    CODE FOR THE Etch-A-Sketch PROJECT

---
+++ script.js
@@ -0,0 +1,37 @@
+const body = document.querySelector('body');
+const btn = document.createElement('button');
+const container = document.createElement('div');
+container.setAttribute('id','container');
+container.style.cssText = "display:flex;flex-direction: column;height:500px;width:500px;border:1px solid black;";
+btn.textContent='Click Me';
+body.appendChild(btn);
+let n;
+btn.addEventListener('click',() => {
+    container.textContent='';
+    n=+prompt('Number of squares per side for the new grid ?');
+    if(n<=100){
+        for(let i=0;i<n;i++){
+            const divRow = document.createElement('div');
+            divRow.style.cssText = "display:flex;flex:1;";
+            for(let j=0;j<n;j++){
+    
+                const div = document.createElement('div');
+                div.style.cssText = "flex:1;";
+                divRow.appendChild(div);
+                div.addEventListener('mouseover',() => {
+                    div.style.backgroundColor = `rgba(${Math.ceil(Math.random()*255)}, ${Math.ceil(Math.random()*255)}, ${Math.ceil(Math.random()*255)},${n+=(n/10)})`;
+                });
+            }
+            container.appendChild(divRow);
+        }
+        body.appendChild(container);
+    }else{
+        prompt('Enter a value lesser or equal to 100');
+    }
+});
+
+
+
+
+
+
